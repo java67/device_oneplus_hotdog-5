6d55c0d293a298f1e69183aa666587490ee004fc
developer <dakaizhe@yeah.net>
2020-10-30 23:46:30 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_hotdog.mk
+    $(LOCAL_DIR)/aosip_hotdog.mk
+COMMON_LUNCH_CHOICES := \
+    aosip_hotdog-userdebug
